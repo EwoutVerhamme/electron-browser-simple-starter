d83f4ce96a7476f9bbe4a2d97aebea49cb08abef
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-03-24 15:43:50 +0100

    first commit

---
+++ public/electron.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+const createMainWindow = () => {
+  let mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      devTools: true,
+    },
+  });
+
+  mainWindow.maximize();
+  mainWindow.webContents.openDevTools();
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.on("new-window", (event, url) => {
+    event.preventDefault();
+    mainWindow.loadURL(url);
+  });
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Electron browser starter
+
+### Start dev-server
+`npm run electron-start`
+### Build production
+`npm run electron-build`
